72d5d4384fdf11e1a1a0b735510fa549926e2afd
chuvakov <4ymail@mail.ru>
2022-01-09 20:10:42 +0300

    init

---
+++ DefectiveGoods/wwwroot/js/Home/Index.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+    var _$createDatepicker = $('#CreateModalDatepicker').datepicker({
+        classes: 'z-i-9999 datepicker--dark',
+    }).data('datepicker');
+
+    // $('#TableItems').DataTable({
+    //     paging: true,
+    //     ordering: true,
+    //     order: [[1,'asc']],
+    //     serverSide: true
+    // });
+});
